7822f4c6b0091456efebb991b1d7b02acadbf72a
mito3103 <105275723+mito3103@users.noreply.github.com>
2022-09-02 06:40:26 +0000

    管理者Topから

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def index
@@ -8,6 +9,17 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
+  def update
+    @customer = Customer.find(customer_params)
+    @customer.update
+    redirect_to admin_customer_path(customer.id)
+  end
+
+private
+  def customer_params
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana, :first_name_kana,:postal_code,:address,:telephone_number,:email,:is_active)
+  end
 end
